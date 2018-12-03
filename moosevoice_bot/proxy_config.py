b3ab57967aa4f304f5cdd24323b8a82d837cf666
CJIOHOED <a.averichev@gmail.com>
2018-12-03 17:43:23 +0300

    fix

---
+++ moosevoice_bot/proxy_config.py
@@ -0,0 +1,4 @@
+user = 'user'
+password = 'password'
+host = '80.211.54.54'
+port = '1080'
--- moosevoice_bot/test.py
+++ moosevoice_bot/test.py
@@ -1,19 +1,8 @@
-# -*- coding: utf-8 -*-
 import config
-import proxy_config as p
-
 import telebot
 
-# import pytelegrambotapi
-
-proxy_user = 'user'
-proxy_password = 'password'
-proxy_host = '80.211.54.54'
-proxy_port = '1080'
-
-telebot.apihelper.proxy = {'https': 'socks5://{0}:{1}@{2}:{3}'.format(p.user, p.password, p.host, p.port)}
 bot = telebot.TeleBot(config.token)
-
+telebot.apihelper.proxy = {'https': 'socks5://user:password@80.211.54.54:1080'}
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
