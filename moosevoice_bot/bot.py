df4be7af9413dea315b006ef706875d406427945
CJIOHOED <a.averichev@gmail.com>
2018-12-03 16:42:19 +0300

    moosevoice_bot

---
+++ moosevoice_bot/bot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+# import pytelegrambotapi
+
+telebot.apihelper.proxy = {'https': 'socks5://user:password@80.211.54.54:1080'}
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
