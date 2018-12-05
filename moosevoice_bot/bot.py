59484abbf532f13137eeb569b4bf28cc35b337ad
Алексей Аверичев <a.averichev@gmail.com>
2018-12-06 01:36:05 +0300

    refactoring

--- moosevoice_bot/bot.py
+++ moosevoice_bot/bot.py
@@ -2,31 +2,43 @@
 
 import telebot
 import logging
-
+import configparser
 import config as cfg
-import proxy_config as prx
 import messages as msg
-
 import anekdot
 
-bot = telebot.TeleBot(cfg.token)
+
+def get_token(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    token = config.get("Telegram", "token")
+    return token
+
+
+def get_proxy(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    protocol = config.get("Proxy", "protocol")
+    user = config.get("Proxy", "user")
+    password = config.get("Proxy", "password")
+    host = config.get("Proxy", "host")
+    port = config.get("Proxy", "port")
+    proxy = f'{protocol}://{user}:{password}@{host}:{port}'
+    return proxy
+
+
+bot = telebot.TeleBot(get_token(cfg.file))
 
 if cfg.proxy:
-    telebot.apihelper.proxy = {'https': '{0}://{1}:{2}@{3}:{4}'
-        .format(prx.protocol,
-                prx.user,
-                prx.password,
-                prx.host,
-                prx.port)}
+    telebot.apihelper.proxy = {'https': get_proxy(cfg.file)}
 
 if cfg.debug:
     logger = telebot.logger
-    telebot.logger.setLevel(logging.DEBUG)  # Outputs debug messages to console.
+    telebot.logger.setLevel(logging.DEBUG)
 
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-
     # Сообщение в нижний регистр
     m = message.text.lower()
 
@@ -34,28 +46,32 @@ def handle_text(message):
     if m in msg.greetings[1:]:
         bot.send_message(message.from_user.id, msg.greetings[0])
 
+    # Отвечает на "Кто ты?"
+    elif m in msg.who_are_you[1:]:
+        bot.send_message(message.from_user.id, msg.who_are_you[0])
+
     # Отвечает на "Как ты?"
     elif m in msg.how_are_you[1:]:
         bot.send_message(message.from_user.id, msg.how_are_you[0])
 
     # elif message.text.lower() == "пришли свое фото":
-        # photo = open('/img/ava.jpg', 'rb')
-        # bot.send_photo(message.from_user.id, photo)
-        # bot.send_photo(message.from_user.id, "FILEID")
+    # photo = open('/img/ava.jpg', 'rb')
+    # bot.send_photo(message.from_user.id, photo)
+    # bot.send_photo(message.from_user.id, "FILEID")
 
     elif m in msg.anekdot[1:]:
         bot.send_message(message.from_user.id, "{0}\n{1}".format(msg.anekdot[0], anekdot.get_anekdot()))
 
     # Проверка на мат из списка
     # elif any(m in s for s in msg.filthy_lang[1:]):
-        # bot.send_message(message.from_user.id, msg.filthy_lang[0])
+    # bot.send_message(message.from_user.id, msg.filthy_lang[0])
 
     # Если не попал в список - не понимает тебя
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю.")
+    # else:
+        # bot.send_message(message.from_user.id, "Я тебя не понимаю.")
 
 
-bot.polling(none_stop=True, interval=0)
+# bot.polling(none_stop=True, interval=0)
 
 
 # Обработчик команд '/start' и '/help'.
@@ -63,12 +79,14 @@ bot.polling(none_stop=True, interval=0)
 def handle_start_help(message):
     pass
 
+
 '''
 @bot.message_handler(commands=['about'])
 def handle_start_help(message):
     bot.send_chat_action(message.chat.id, 'typing')
     bot.reply_to(message, 'АЗ ЕСЬМ ЛОСЬ')
 '''
+
 
 # Обработчик для документов и аудиофайлов
 @bot.message_handler(content_types=['document', 'audio'])
--- moosevoice_bot/config.py
+++ moosevoice_bot/config.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 token = '784308141:AAF53iiSfoE-_wf0XRRwRNfJ4j4WCgmaU5M'
+file = "config.ini"
 proxy = True
 debug = False
