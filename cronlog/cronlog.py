0061aba10cca49f2a855e4366a81a02f0cb73824
Алексей Аверичев <a.averichev@gmail.com>
2018-12-13 23:24:35 +0300

    refactoring & cleaning

--- cronlog/cronlog.py
+++ cronlog/cronlog.py
@@ -4,10 +4,7 @@
 from csv import reader as csv_reader
 from sys import argv as argument
 from argparse import ArgumentParser
-from time import time
 import re
-from datetime import datetime
-# from __future__ import with_statement
 
 
 def argParser():
@@ -18,41 +15,36 @@ def argParser():
     return parser
 
 
-def get_last_lines(path_to_file, count=10, reverse=True):
+def get_last_lines(path_to_file, count=100, reverse=True):
     with open(path_to_file, "r") as f:
-        f.seek(0, 2)  # Seek @ EOF
-        file_size = f.tell()  # Get Size
-        f.seek(max(file_size - 1024, 0), 0)  # Set pos @ last n chars
-        lines = f.readlines()  # Read to end
-
-    lines = lines[-count:]  # Get last 10 lines
-    lines = lines[::-1] if reverse else lines  # Get reverse
+        f.seek(0, 2)
+        file_size = f.tell()
+        f.seek(max(file_size - 1024, 0), 0)
+        lines = f.readlines()
+    lines = lines[-count:]
+    lines = lines[::-1] if reverse else lines
     return lines
 
 
-def get_runtime(finds, lines):
-    _stroke = ''
+def get_runtime(f_str, lines, status='STOP'):
+    _result = ''
     for _line in lines:
-        if finds in _line:
-            _stroke = _line
+        if f_str in _line and status in _line:
+            _result = _line
             break
+    if not _result:
+        return ''
     _rgx_runtime = r'(\d*\.)?\d*(?=\)(?!\)))'
-    _runtime = re.search(_rgx_runtime, _stroke)
-    _runtime = _runtime.group().replace('(', '').replace(')', '') if _runtime else '-'
+    _runtime = re.search(_rgx_runtime, _result)
+    _runtime = _runtime.group().replace('(', '').replace(')', '') if _runtime else ''
     return _runtime
 
 
 if __name__ == "__main__":
-
-    parser = argParser()
-    namespace = parser.parse_args(argument[1:])
-    fname = 'events.log'
-    # fname = namespace.path
-    find_str = "import_kor_product.php"
-    # find_str = namespace.name
-    stroke = ''
-
-    last_lines = get_last_lines(fname)
+    pars = argParser()
+    namespace = pars.parse_args(argument[1:])
+    path2file = namespace.path
+    find_str = namespace.name
+    last_lines = get_last_lines(path2file)
     runtime = get_runtime(find_str, last_lines)
-
     print(runtime)
